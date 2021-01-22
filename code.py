455bfeeba76eb4d404320c4ee35c9bbcdde22f83
Yunyang-syy <59624303+Yunyang-syy@users.noreply.github.com>
2021-01-23 01:27:46 +0800

    update code.py

--- code.py
+++ code.py
@@ -1,10 +1,20 @@
 def are_valid_groups(students, groups):
+	l = []
+	a=0
 	for i in students:
-		for j in groups:
-			if i in j:
-				break
-			if j == groups[-1]:
-				return False
-		if i == students[-1]:
+		a+=1
+		if i in l:
+			return False
+		else:
+			l.append(i)
+			for j in groups:
+				if len(j) == 2 or len(j) == 3:
+					if i in j:
+						break
+					if j == groups[-1]:
+						return False
+				else:
+					return False
+		if len(students) == a:
 			return True
 	return False 
