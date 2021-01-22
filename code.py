cc145c8d961f649b9db681b8d426709c1a395149
Yunyang-syy <59624303+Yunyang-syy@users.noreply.github.com>
2021-01-23 01:18:16 +0800

    code.py writen by yunyang

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
- 
+def are_valid_groups(students, groups):
+	for i in students:
+		for j in groups:
+			if i in j:
+				break
+			if j == groups[-1]:
+				return False
+		if i == students[-1]:
+			return True
+	return False 
